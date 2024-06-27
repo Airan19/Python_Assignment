78565f439a7adbdcbfe34ac082a23d2c7ae926af
Utkarsh Mukul <wst06@banorcapital.com>
2024-06-27 08:27:33 +0100

    Added logs

--- bugfix_1/bonds.py
+++ bugfix_1/bonds.py
@@ -1,4 +1,5 @@
 from datetime import datetime
 
 def get_summary(fund_code, ts=datetime.now().strftime('%Y-%m-%d %H:%M:%S')):
+    # Assume below line is writing data to DB
     print(f"BONDS -> {fund_code} is Loaded at time : {ts}")
