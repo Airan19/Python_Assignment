35e8ba5d7c8e010836426d6bc1aea7361431cd23
Utkarsh Mukul <wst06@banorcapital.com>
2024-06-28 14:40:48 +0100

    Changes

--- redis_task/consumer.py
+++ redis_task/consumer.py
@@ -2,6 +2,7 @@ import redis
 import time
 import json
 import os
+from logger import Logger as log
 from db import DatabaseManager
 from dotenv import load_dotenv
 
@@ -55,40 +56,31 @@ def execute_sql_query(query, params=None, fetchone=False, fetchall=False):
         output = sql_db.execute_query(query, params, fetchone, fetchall)
         return output
 
-
-def log_change(event, key, data):
-    """
-    Log changes to a file with the event type, key, and data.
-    """
-    with open('employee_log.txt', 'a') as log_file:
-        log_file.write(f"{time.ctime()}: {event} - Key: {key}  Data: {data} \n")
-
-
 def handle_set_action(r_conn, key):
     """
     Handles the 'set' event for a Redis key, updating or inserting the data into the database.
     """
     # Retrieve data from Redis
-    data = r_conn.get(f"employee/{key}")
+    data = r_conn.get(key)
     data = data.decode('utf-8') if isinstance(data, bytes) else print(type(data))
     if isinstance(data, str):
-        data = json.loads(r_conn.get(f"employee/{key}"))
+        data = json.loads(r_conn.get(key))
     if data:
         # SQL query to update or insert the employee record
         table_name = os.getenv('DB_TABLE_NAME')
         query = f"""
         IF EXISTS (SELECT 1 FROM %s WHERE emp_id=%s)
-        UPDATE Employees
+        UPDATE %s
         SET name=%s, gender=%s, phone=%s, department=%s, date_of_birth=%s, email=%s, experience=%s
         WHERE emp_id=%s
         ELSE
-        INSERT INTO Employees (emp_id, name, gender, phone, department, date_of_birth, email, experience)
+        INSERT INTO %s (emp_id, name, gender, phone, department, date_of_birth, email, experience)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """
         params = (
             table_name,
-            data["emp_id"], data['name'], data['gender'], data['phone'], data['department'],
-            data['date_of_birth'], data['email'], data['experience'], data['emp_id'], data['emp_id'],
+            data["emp_id"], table_name, data['name'], data['gender'], data['phone'], data['department'],
+            data['date_of_birth'], data['email'], data['experience'], data['emp_id'], table_name, data['emp_id'],
             data['name'], data['gender'], data['phone'], data['department'],
             data['date_of_birth'], data['email'], data['experience']
         ) 
@@ -101,9 +93,10 @@ def handle_delete_action(key):
     Handles the 'del' event for a Redis key, deleting the corresponding data from the database.
     """
     # SQL query to delete the employee record
-    employee_id = int(key)
-    query = "DELETE FROM Employees WHERE id=%s"
-    params = (employee_id)
+    table_name = os.getenv('DB_TABLE_NAME')
+    employee_id = key.split('/')[1]
+    query = "DELETE FROM %s WHERE emp_id=%s"
+    params = (table_name, employee_id)
 
     # Execute the SQL query
     execute_sql_query(query, params)
@@ -120,18 +113,14 @@ def listen_for_messages():
         try:
             if message['type'] == 'pmessage':
                 event = message['data'].decode('utf-8') if isinstance(message['data'], bytes) else str(message['data'])
-                key = message['channel'].decode('utf-8').split('/')[-1]
+                key = message['channel'].decode('utf-8').split(":")[-1]
+                log.info(f"Logged change: {event} - Key: {key}")
                 # Handle 'set' event
                 if event == 'set':
                     handle_set_action(r_conn, key)
                 # Handle 'del' event
                 elif event == 'del':
                     handle_delete_action(key)
-                data = r_conn.get(f"employee/{key}")
-
-                # Log the change
-                log_change(event, key, data)
-                print(f"Logged change: {event} - Key: {key}  Data: {data}")
         except Exception as e:
             print(f"Error processing message: {e}")
 
